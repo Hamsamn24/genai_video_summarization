55b2b5610880858206ba3a11695abd39d96c80d6
Hamsa M N <158328265+Hamsamn24@users.noreply.github.com>
2025-04-23 18:03:19 +0530

    Update genAI_Video_Summary_Final.py

--- genAI_Video_Summary_Final.py
+++ genAI_Video_Summary_Final.py
@@ -78,7 +78,7 @@ def turn_into_story(summary):
     return model.invoke(prompt)
 
 st.title("Hamsa - YouTube/Upload Video Summarizer Using Groq LLM")
-st.image("butterfly.png")
+st.image("ipl.png")
 
 youtube_url = st.text_input("Paste a YouTube video URL:", placeholder="https://www.youtube.com/watch?v=example")
 
